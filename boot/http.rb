74cd200c307421c3b2c2b0f4e2187f638546f69f
Miles Georgi <azimux@gmail.com>
2026-02-13 16:52:14 -0800

    Reduce lines in http.rb a bit

--- boot/http.rb
+++ boot/http.rb
@@ -3,20 +3,16 @@ require "foobara/entities_plumbing"
 require "foobara/auth_http"
 require "foobara_demo/blog_auth"
 
-env = ENV["FOOBARA_ENV"]
-secure = env != "development" && env != "test"
+RACK_CONNECTOR = Foobara::CommandConnectors::Http::Rack.new(
+  authenticator: [:bearer, :api_key],
+  auth_map: {
+    blog_auth_user: FoobaraDemo::BlogAuth::User,
+    blog_user: FoobaraDemo::BlogAuth::FindBlogUserForAuthUser
+  }
+) do
+  register_allowed_rule :is_article_author, -> { blog_user == article.author }
+  register_allowed_rule :is_author, -> { blog_user == author }
 
-connector_class = Foobara::CommandConnectors::Http::Rack
-# TODO: we should be able to register these on an instance inside the block passed to .new
-connector_class.register_allowed_rule :is_article_author, -> { blog_user == article.author }
-connector_class.register_allowed_rule :is_author, -> { blog_user == author }
-
-auth_map = {
-  blog_auth_user: FoobaraDemo::BlogAuth::User,
-  blog_user: FoobaraDemo::BlogAuth::FindBlogUserForAuthUser
-}
-
-RACK_CONNECTOR = connector_class.new(authenticator: [:bearer, :api_key], auth_map:) do
   # TODO: we need a reusable way to reuse allowed rules across connectors
   command FoobaraDemo::Blog::StartNewArticle, allow_if: :is_author,
                                               request: { default: { author: -> { blog_user } } }
@@ -40,16 +36,17 @@ RACK_CONNECTOR = connector_class.new(authenticator: [:bearer, :api_key], auth_ma
           :aggregate_entities,
           request: { set: { user: -> { blog_auth_user } } }
 
-  set_user_to_auth_user = { set: { user: -> { authenticated_user } } }
+  env = ENV["FOOBARA_ENV"]
   login_response_mutators = [
-    Foobara::AuthHttp::MoveRefreshTokenToCookie.new(secure:),
+    Foobara::AuthHttp::MoveRefreshTokenToCookie.new(secure: env != "development" && env != "test"),
     Foobara::AuthHttp::MoveAccessTokenToHeader
   ]
+
   command Foobara::Auth::CreateApiKey,
           :auth,
           inputs: Foobara::AttributesTransformers.reject(:needs_approval),
-          request: set_user_to_auth_user
-  command Foobara::Auth::GetApiKeySummaries, :auth, request: set_user_to_auth_user
+          request: { set: { user: -> { authenticated_user } } }
+  command Foobara::Auth::GetApiKeySummaries, :auth, request: { set: { user: -> { authenticated_user } } }
   command Foobara::Auth::Login, inputs: { only: [:username_or_email, :plaintext_password] },
                                 response: login_response_mutators
   command Foobara::Auth::RefreshLogin, request: Foobara::AuthHttp::SetRefreshTokenFromCookie,
