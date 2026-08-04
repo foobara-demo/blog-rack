271be43d80e941b5d9d38a3a2cbac7695fff9964
Miles Georgi <azimux@gmail.com>
2026-08-03 17:32:11 -0700

    use redis in prod

--- boot/crud.rb
+++ boot/crud.rb
@@ -1,12 +1,12 @@
 Foobara::Persistence.default_crud_driver = case ENV.fetch("FOOBARA_ENV")
                                            when "test"
                                              Foobara::Persistence::CrudDrivers::InMemory.new
-                                           else
+                                           when "development"
                                              # :nocov:
-                                             # require "foobara/redis_crud_driver"
-                                             # Foobara::RedisCrudDriver.new
-
                                              require "foobara/local_files_crud_driver"
                                              Foobara::LocalFilesCrudDriver.new
+                                           else
+                                             require "foobara/redis_crud_driver"
+                                             Foobara::RedisCrudDriver.new
                                              # :nocov:
                                            end
